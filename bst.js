d3d4e6123cac5cd963e0810938872555cf6be789
adithyanarayanan <adithyanarayanansajan@gmail.com>
2025-06-09 11:36:24 +0530

    implement delete function

--- bst.js
+++ bst.js
@@ -42,9 +42,69 @@ function createTree(array) {
             }
         }
     }
+    function deleteItem(val) {
+        currentNode = rootNode;
+        var previousNode = null;
+        while (currentNode !== null && currentNode.data !== val) {
+            previousNode = currentNode
+            if (val > currentNode.data) {
+                currentNode = currentNode.right;
+            }
+            else {
+                currentNode = currentNode.left;
+            }
+        }
+        if (currentNode === null) {
+            console.log("no node");
+            return;
+        }
+        // No child
+        if (currentNode.left == null && currentNode.right == null) {
+            if (currentNode === rootNode)
+                rootNode = null
+            else if (currentNode.data > previousNode.data)
+                previousNode.right = null;
+            else
+                previousNode.left = null;
+        }
+        // left subTree empty
+        else if (currentNode.left == null) {
+            if (currentNode == rootNode)
+                rootNode = currentNode.right;
+            else if (previousNode.right == currentNode)
+                previousNode.right = currentNode.right;
+            else
+                previousNode.left = currentNode.right;
+        }
+        // right subtree empty
+        else if (currentNode.right == null) {
+            if (currentNode == rootNode)
+                rootNode = currentNode.left
+            else if (previousNode.right == currentNode)
+                previousNode.right = currentNode.left;
+            else
+                previousNode.left = currentNode.left;
+        }
+        // two children
+        else {
+            let inOrderPrev = currentNode;
+            let inOrderNode = currentNode.right;
+            while (inOrderNode.left !== null) {
+                inOrderPrev = inOrderNode;
+                inOrderNode = inOrderNode.left;
+            }
+            currentNode.data = inOrderNode.data;
+            if (inOrderPrev == currentNode) {
+                inOrderPrev.right = inOrderNode.right;
+            }
+            else
+                inOrderPrev.left = inOrderNode.right;
+        }
+    }
     return {
         rootNode,
         insert,
+        deleteItem,
     }
 }
 
@@ -65,4 +125,5 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 array = [1, 2, 3, 4, 5];
 tree = createTree(array);
 tree.insert(7)
+tree.deleteItem(4)
 prettyPrint(tree.rootNode);
